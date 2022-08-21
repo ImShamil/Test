41bdd723816c2225650ea8afb81d23d09b547060
Imandinov_Shamil <flip0173@gmail.com>
2022-08-21 14:25:42 +0400

    add js for conversation

--- js/conversation.js
+++ js/conversation.js
@@ -1,26 +1,34 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let el_form = document.querySelector('form');
-    if (!el_form) return;
+    if (!el_form) return;    
 
-    let el_message = document.querySelector('.messages-list__box');
-    let last_user;
-    let last_date;
+    let last_user="";
+    let last_date="";
+    
     let messagesList=[
        {
            date:"20 октября 2017",
            messages:[
-               {
-                   user:"left",
-                   message:"Это означает, что в Вашем доме, офисе, кабинете, мастерской хранятся ценности бóльшие, чем просто деньги или драгоценности, - культурные ценности. Произведения искусства, антиквариат, иные предметы коллекционирования – все это имущество, безусловно высокая стоимость которого."
-               },
-               {
-                   user:"right",
-                   message:"Это означает,"
-               },
-               {
-                   user:"left",
-                   message:"доме, офисе, кабинете, мастерской"
-               }
+            {
+                user:"left",
+                message:"Это контент для отступа"
+            },
+            {
+                user:"right",
+                message:"Это контент для отступа"
+            },
+            {
+               user:"left",
+               message:"Это означает, что в Вашем доме, офисе, кабинете, мастерской хранятся ценности бóльшие, чем просто деньги или драгоценности, - культурные ценности. Произведения искусства, антиквариат, иные предметы коллекционирования – все это имущество, безусловно высокая стоимость которого."
+            },
+            {
+               user:"right",
+               message:"Это означает,"
+            },
+            {
+               user:"left",
+               message:"доме, офисе, кабинете, мастерской"
+            }
            ]
        },
        {
@@ -38,103 +46,83 @@ document.addEventListener('DOMContentLoaded', ()=>{
        }
        
    ];
+
+   let insertDateBox=(date)=>{
+    if(date!=last_date){
+        last_date=date;
+        let messagesListBox = document.querySelector('.messages-list__box');
+        let dateMessage=
+        `<div class="date-message">
+             <div class="date-message__box">
+                 <div class="date-message__date">
+                     ${date}
+                 </div>
+             </div>
+        </div>`;
+        if(messagesListBox){
+            messagesListBox.insertAdjacentHTML("beforeend",dateMessage);
+        }
+    }
+   }
+
+   let insertMessage=(user, message)=>{
+    let userBox;
+    last_user=user;
+    switch(user){
+        case 'right':
+            userBox=
+            `<div class="user user--right">
+                <div class="user__box user__box--right">
+                    <div class="user__logo user__logo--right"><img src="images/user-logo.png" alt="user-logo"> </div>
+                    <div class="user__message-text user__message-text--right">${message}</div>
+                </div>
+            </div>`;
+            break;
+        case 'left':
+            userBox=
+            `<div class="user user--left">
+                <div class="user__box user__box--left">
+                    <div class="user__logo user__logo--left"> <img src="images/user-logo.png" alt="user-logo"> </div>
+                    <div class="user__message-text user__message-text--left">${message}</div>
+                </div>
+            </div>`
+    }
+    let dateMessageBox=document.querySelector('.date-message:last-child>.date-message__box');
+    if(dateMessageBox){
+        dateMessageBox.insertAdjacentHTML("beforeend",userBox);
+    }
+   }
+   
+   let autoScroll=()=>{
+    let messagesListBox=document.querySelector('.messages-list__box');
+    messagesListBox.scrollTop=messagesListBox.scrollHeight;
+   }
    
    messagesList.forEach((item)=>{
        let date=item.date;
-       last_date=date;
-       console.log(date);
-       let dateBox=`
-       <div class="date-message">
-            <div class="date-message__box">
-                <div class="date-message__date">
-                    ${date}
-                </div>
-            </div>
-       </div>`;
-       console.log(dateBox);
-   
-      if(el_message){
-       el_message.insertAdjacentHTML("beforeend",dateBox);
-      }
        let messages=item.messages;
+       insertDateBox(date);
        messages.forEach((item)=>{
            let user=item.user;
-           last_user=user;
            let message=item.message;
-           let messageBox;
-           if (user=='right'){
-                messageBox=
-                `<div class="user user--right">
-                    <div class="user__box user__box--right">
-                        <div class="user__logo user__logo--right"><img src="images/user-logo.png" alt="user-logo"> </div>
-                        <div class="user__message-text user__message-text--right">${message}</div>
-                    </div>
-                </div>`
-           }
-           if (user=='left'){
-                messageBox=`
-                <div class="user user--left">
-                    <div class="user__box user__box--left">
-                        <div class="user__logo user__logo--left"> <img src="images/user-logo.png" alt="user-logo"> </div>
-                        <div class="user__message-text user__message-text--left">${message}</div>
-                    </div>
-                </div>`
-            }
-           let box=document.querySelector('.messages-list__box>.date-message:last-child>.date-message__box');
-           if(box){
-            box.insertAdjacentHTML("beforeend",messageBox);
-           }
+           insertMessage(user,message);
        });
    });
+   
+   window.onload=autoScroll;
+   
     el_form.addEventListener('submit', (e)=>{
         e.preventDefault()
-        let your_message = el_form.querySelector('[name="user-message"]').value;
-
+        let message = el_form.querySelector('[name="user-message"]').value;
         let date = new Date().toLocaleString('default', {day:"numeric",month: 'long',year:'numeric' }).slice(0,-3);
-
-        if(your_message){
+        if(message){
             let user=(last_user=='right')?'left':'right';
-                last_user=user; 
-            if(date!=last_date){
-                let dateBox=`
-                <div class="date-message">
-                     <div class="date-message__box">
-                         <div class="date-message__date">
-                             ${date}
-                         </div>
-                     </div>
-                </div>`;
-                if(el_message){
-                    el_message.insertAdjacentHTML("beforeend",dateBox);
-                   }
-                   if (user=='right'){
-                    messageBox=
-                    `<div class="user user--right">
-                        <div class="user__box user__box--right">
-                            <div class="user__logo user__logo--right"><img src="images/user-logo.png" alt="user-logo"> </div>
-                            <div class="user__message-text user__message-text--right">${your_message}</div>
-                        </div>
-                    </div>`
-               }
-               if (user=='left'){
-                    messageBox=`
-                    <div class="user user--left">
-                        <div class="user__box user__box--left">
-                            <div class="user__logo user__logo--left"> <img src="images/user-logo.png" alt="user-logo"> </div>
-                            <div class="user__message-text user__message-text--left">${your_message}</div>
-                        </div>
-                    </div>`
-                }
-                let box=document.querySelector('.messages-list__box>.date-message:last-child>.date-message__box');
-                if(box){
-                 box.insertAdjacentHTML("beforeend",messageBox);
-                }
-
-            }
-            else{
-
-            }
+            insertDateBox(date);
+            insertMessage(user,message);
+            autoScroll();
         }
         
-    })
+    });
+    
 })
+
