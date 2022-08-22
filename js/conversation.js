b9e1cabb71293c6e52b6c7b55c735e46ce05c574
Imandinov Shamil <flip0173@gmail.com>
2022-08-22 07:25:23 +0400

    fix

--- js/conversation.js
+++ js/conversation.js
@@ -46,55 +46,6 @@ document.addEventListener('DOMContentLoaded', function(){
        }
        
    ];
-
-   function insertDateBox(date){
-    if(date!=last_date){
-        last_date=date;
-        let messagesListBox = document.querySelector('.messages-list__box');
-        let dateMessage=
-        '<div class="date-message"><div class="date-message__box"><div class="date-message__date">'
-        +
-        date
-        +
-        '</div></div></div>';
-        if(messagesListBox){
-            messagesListBox.insertAdjacentHTML("beforeend",dateMessage);
-        }
-    }
-   }
-
-   function insertMessage(user, message){
-        let userBox;
-        last_user=user;
-        switch(user){
-            case 'right':
-                userBox='<div class="user user--right"><div class="user__box user__box--right"><div class="user__logo user__logo--right"><img src="images/user-logo.png" alt="user-logo"> </div><div class="user__message-text user__message-text--right">'
-                +
-                message
-                +
-                '</div></div></div>';
-                break;
-            case 'left':
-                userBox=
-                '<div class="user user--left"><div class="user__box user__box--left"><div class="user__logo user__logo--left"> <img src="images/user-logo.png" alt="user-logo"> </div><div class="user__message-text user__message-text--left">'
-                +
-                message
-                +
-                '</div></div></div>'
-        }
-
-    let dateMessageBox=document.querySelector('.date-message:last-child>.date-message__box');
-    
-    if(dateMessageBox){
-        dateMessageBox.insertAdjacentHTML("beforeend",userBox);
-    }
-   }
-   
-    function autoScroll(){
-        let messagesListBox=document.querySelector('.messages-list__box');
-        messagesListBox.scrollTop=messagesListBox.scrollHeight;
-   }
-   
    messagesList.forEach(function(item){
        let date=item.date;
        let messages=item.messages;
@@ -120,5 +71,70 @@ document.addEventListener('DOMContentLoaded', function(){
             autoScroll();
         }
     });
+
+   function insertDateBox(date){
+    if(date!=last_date){
+        last_date=date;
+        let messagesListBox = document.querySelector('.messages-list__box');
+        let dateMessage=
+        '<div class="date-message"><div class="date-message__box"><div class="date-message__date">'
+        +
+        date
+        +
+        '</div></div></div>';
+        if(messagesListBox){
+            messagesListBox.insertAdjacentHTML("beforeend",dateMessage);
+        }
+    }
+   }
+
+   function insertMessage(user, message){
+    let userBox;
+    last_user=user;
+    switch(user){
+        case 'right':
+            userBox='<div class="user user--right"><div class="user__box user__box--right"><div class="user__logo user__logo--right"><img src="images/user-logo.png" alt="user-logo"> </div><div class="user__message-text user__message-text--right">'
+            +
+            message
+            +
+            '</div></div></div>';
+            break;
+        case 'left':
+            userBox=
+            '<div class="user user--left"><div class="user__box user__box--left"><div class="user__logo user__logo--left"> <img src="images/user-logo.png" alt="user-logo"> </div><div class="user__message-text user__message-text--left">'
+            +
+            message
+            +
+            '</div></div></div>'
+    }
+
+    let dateMessageBox=document.querySelector('.date-message:last-child>.date-message__box');
+
+    if(dateMessageBox){
+        dateMessageBox.insertAdjacentHTML("beforeend",userBox);
+    }
+}
+
+   function insertDateBox(date){
+    if(date!=last_date){
+        last_date=date;
+        let messagesListBox = document.querySelector('.messages-list__box');
+        let dateMessage=
+        '<div class="date-message"><div class="date-message__box"><div class="date-message__date">'
+        +
+        date
+        +
+        '</div></div></div>';
+        if(messagesListBox){
+            messagesListBox.insertAdjacentHTML("beforeend",dateMessage);
+        }
+    }
+   }
+    
+    function autoScroll(){
+        let messagesListBox=document.querySelector('.messages-list__box');
+        messagesListBox.scrollTop=messagesListBox.scrollHeight;
+     }
+
 })
 
